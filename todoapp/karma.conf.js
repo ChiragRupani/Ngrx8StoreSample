c546d4ea66a3637b3962ee8cb5fa35e8ddd7df9a
Chirag Rupani <36813124+chiragrupani@users.noreply.github.com>
2022-01-18 10:56:54 +0530

    Updated Angular to 13.1

--- todoapp/karma.conf.js
+++ todoapp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Edge"],
+    browsers: ["EdgeAny"],
     singleRun: false,
     restartOnFileChange: true,
   });
